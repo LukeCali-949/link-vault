555fc50629730e0fcfce100453a9e7f345ab916e
LukeCali-949 <mengluke17@gmail.com>
2023-08-01 03:43:29 -0500

    Day 1 - 4 hour session got to much done to nameall

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,16 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import supabase from "@/lib/supabaseClient";
-import { useRouter } from 'next/navigation'
+import { User } from "@supabase/supabase-js";
+import AuthForm from "./auth-form";
+
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  let userProfile;
-
-  useEffect(() => {
-    const {
-      data: { user },
-    } = await supabase.auth.getUser();
-
-    const { data, error } = await supabase.auth.getSession();
-
-    userProfile = user;
-    console.log("SPACE");
-    console.log(data);
-  }, [])
-
-
   return (
-    {userProfile === null ? (<h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>) : }
-    
+    <div>
+      <AuthForm />
+    </div>
   );
 }
